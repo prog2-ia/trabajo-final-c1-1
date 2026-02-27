58ce6736d93a7aa6c941936092e34837f771ddbc
Adrián <a.gosalbezortiz@gmail.com>
2026-02-27 14:17:41 +0100

    Actualizar el archivo ClaseTratamiento

--- ClaseTratamiento.py
+++ ClaseTratamiento.py
@@ -1,4 +1,8 @@
 class Tratamiento:
     def __init__(self, tipo, utilidad):
-        self.tipo = tipo
-        selfutilidad = utilidad
+        self.tipo = tipo #El tipo de tratamiento que es
+        self.tipo.append(tipo)
+        self.utilidad = utilidad #Para que sirve ese tratamiento
+
+    def descripcion(self):
+        print(f'El tratamiento es del tipo {self.tipo} y su utilidad es {self.utilidad}')
