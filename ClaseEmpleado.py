be049e5d752408b15238f8384786fea65b1da1e3
Guillermo <wornino@gmail.com>
2026-03-06 14:35:52 +0100

    pequeña actualización de la clase Empleados

--- ClaseEmpleado.py
+++ ClaseEmpleado.py
@@ -1,10 +1,11 @@
 class Empleado:
-    def __init__(self, nombre, edad, genero, dni, animal, tiempo):
+    def __init__(self, nombre, edad, genero, dni, refugio = None):
         self.nombre = nombre
         self.edad = edad
         self.genero = genero
         self.dni = dni
-        self.animal = animal #animal del cual se encarga en este momento
-        self.tiempo = tiempo #timepo dedicado al animal con el que está ahora
     def HorasTrabajadas (self, horas):
         self.tiempo += horas
+
+    if refugio:
+        refugio.añadir_empleado(self)
